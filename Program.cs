a07a9e5ab7a5d3387bdfeac3c922b030b0e220fa
democraz20 <mozartrasig@gmail.com>
2022-04-06 01:21:09 +0700

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace taskmd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach (var arg in args)
+            {
+              Console.WriteLine(arg);
+            }
+            Console.Title = "TaskMD";
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+        }
+    }
+}
