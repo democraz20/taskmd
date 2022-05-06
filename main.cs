c3f6388345aea039d4025976f70af548f08ea9ef
democraz20 <mozartrasig@gmail.com>
2022-05-06 23:10:57 +0700

    added usage

--- main.cs
+++ main.cs
@@ -14,27 +14,38 @@ public class taskmd
 {
     public static void M(string[] args)
     {
-        switch(args[0]){
-            case "init":
-                if (args.Length == 3){
-                    init.start(args[1], args[2]);
-                }else if (args.Length == 2){
-                    init.start(args[1]);}
-                else{init.start();}
-                break;
-            case "read": //its ugly i know
-                if (args.Length == 1){read.readT();}
-                else{read.readT(args[1]);}
-                break;
-            case "add":
-                if (args.Length == 1){Console.WriteLine(" Missing arguements");}
-                else{add.addT(args[1]);}
-                break;
-            case "del":
-                if (args.Length == 1){Console.WriteLine(" Missing arguements");}
-                else{delete.delT(int.Parse(args[1]));}
-                break;
-
+        if(args.Length == 0){ //usage
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine(" Usage: [required] (optional)");
+            Console.WriteLine(" Commands: init (file header) (file name)");
+            Console.WriteLine("         : add  [task] (file name)");
+            Console.WriteLine("         : del  [task index] (file name)");
+            Console.WriteLine("         : tog [task index] (file name)");
+            Console.WriteLine("         : read");
+            Console.ResetColor();
+        }
+        else{
+            switch(args[0]){
+                case "init":
+                    if (args.Length == 3){
+                        init.start(args[1], args[2]);
+                    }else if (args.Length == 2){
+                        init.start(args[1]);}
+                    else{init.start();}
+                    break;
+                case "read": //its ugly i know
+                    if (args.Length == 1){read.readT();}
+                    else{read.readT(args[1]);}
+                    break;
+                case "add":
+                    if (args.Length == 1){Console.WriteLine(" Missing arguements");}
+                    else{add.addT(args[1]);}
+                    break;
+                case "del":
+                    if (args.Length == 1){Console.WriteLine(" Missing arguements");}
+                    else{delete.delT(int.Parse(args[1]));}
+                    break;
+            }
         }
     }
 
